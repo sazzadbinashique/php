3bc9a7941f3143a4d65fa914ea54ff45947b3a99
Sazzad Bin Ashique <sazzad.sumon35@gmail.com>
2018-07-15 12:48:24 +0600

    Design pattern iterator

---
+++ book/chapter4/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>This is chapter three</title>
+</head>
+<body>
+
+
+
+<div >
+	
+	<ul style="text-align: center;" >
+		<li ><a href="iterator.php">Iterator pattern</a></li>
+	</ul>
+	
+</div>
+
+</body>
+</html>
