2432099f47419d9185e3831eb6109007b0064a25
Sazzad Bin Ashique <sazzad.sumon35@gmail.com>
2018-07-25 08:36:08 +0600

    Json data type & creating Object

--- book/chapter4/index.php
+++ book/chapter4/index.php
@@ -13,6 +13,9 @@
 		<li ><a href="iterator.php">Iterator pattern</a></li>
 		<li ><a href="observable.php">Observer pattern</a></li>
 		<li ><a href="reflection.php">Reflection Class</a></li>
+		<li ><a href="example.json">Json First program</a></li>
+		<li ><a href="json.html">Json second program</a></li>
+		<li ><a href="json_object.html">Json Object Example</a></li>
 	</ul>
 	
 </div>
