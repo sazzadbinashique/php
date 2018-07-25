c0fe5e50e4b1d81b847da01a99a1c8493aba1f42
Sazzad Bin Ashique <sazzad.sumon35@gmail.com>
2018-07-25 18:27:24 +0600

    form add

---
+++ form/index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Form</title>
+  </head>
+  <body>
+
+     <div class="container">
+        <h2 align="center">This Add form </h2>
+         <form action="">
+            <div class="form-group">
+              <label for="name">Full Name:</label>
+              <input type="text" class="form-control" placeholder="বাংলায় পুরো নাম লিখুন।" id="name">
+            </div>
+            <div class="form-group">
+              <label for="birthday">Birthday:</label>
+              <input type="text" class="form-control" id="birthday">
+            </div>
+            <div class="checkbox">
+              <label><input type="checkbox"> Remember me</label>
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form> 
+     </div>
+
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script type='text/javascript' src='assets/datetimepicker/jquery.datetimepicker.min.js?ver=2.4.6'></script>
+
+  </body>
+</html>
