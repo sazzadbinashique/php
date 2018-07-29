a4fe311dc642f86927750a8a98104c6f76b2250a
Sazzad Bin Ashique <sazzad.sumon35@gmail.com>
2018-07-29 18:49:30 +0600

    single && post && put && delete

--- slimapp/src/config/db.php
+++ slimapp/src/config/db.php
@@ -13,7 +13,7 @@ class db{
 	public function connect (){
 		$mysql_connect_str = "mysql:host=$this->dbhost; dbname=$this->dbname";
 		$dbconnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass); 
-		$dbconnection ->setAttribute(PDO::ATT_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+		$dbconnection ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 		return $dbconnection;
 	}
 }
