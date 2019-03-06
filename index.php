4751b1aef10d3bdc34d7b96551a6c21513425e6c
Sazzad Bin Ashique <sazzad.sumon35@gmail.com>
2019-03-06 10:30:17 +0600

    some file delete

--- index.php
+++ index.php
@@ -1,43 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>PHP-> Sazzad Bin Ashique</title>
-</head>
-<body>
-	
-<?php 
-// class Car {
-// 	public $comp; 
-// 	public $color = 'red';
-// 	public $hasSunRoof = true; 
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-// 	public function hello(){
-// 		return "Hello world";
-// 	}
-// }
-
-// $bmw = new Car();
-// $mercedes = new Car();
-
-// echo $bmw -> hello();
-// echo "\n";
-// echo $mercedes -> hello();
-?>
-
-<div>
-	<ul style="text-align: center;">
-		<li><a href="../php/OOPCRUD/index.php">OOPCRUD</a></li>
-		<li><a href="../php/book">book</a></li>
-		<li><a href="../php/demoapi">demoapi</a></li>
-		<li><a href="../php/form">form</a></li>
-		<li><a href="../php/ifelsestatement">ifelsestatement</a></li>
-		<li><a href="../php/image_upload">image_upload</a></li>
-		<li><a href="../php/oop/index.php">oop</a></li>
-		<li><a href="../php/slimapp">slimapp</a></li>
-		<li><a href="../php/phpstirnfunction">PHP Strin Function</a></li>
-	</ul>
-</div>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>PHP-Language</title>
+  </head>
+  <body>
 
 
-</body>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  </body>
 </html>
